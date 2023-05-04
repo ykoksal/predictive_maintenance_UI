87f11291fe2e444ef49d3369e0bf6b8748e58d2a
Mehmet Yiğit Köksal <yigitkoksal8@gmail.com>
2023-05-04 15:54:55 +0300

    -revise dashboards, logo, top bar\n -remove unnecessary sidebar items\n -create original index2 and index3 html files for later use\n -complete "TODO: Make charts to appear after refreshing the page, this resolved the issue of not displaying the charts on the initial startup of the pages."

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask, render_template
 import plotly.express as px
 import pandas as pd
 import numpy as np
-
+import plotly.graph_objs as go
+import plotly.offline as opy
 app = Flask(__name__)
 
 @app.route('/')
@@ -22,12 +23,26 @@ def index():
         'value': (20 + 2 * np.random.randn(100)).cumsum()
     })
 
-    time_series_chart = px.line(time_series_data, x='date', y='value')
+    # time_series_chart = px.line(time_series_data, x='date', y='value')
 
-    return render_template('index.html', bar_chart=bar_chart.to_html(), time_series_chart=time_series_chart.to_html())
+    time_series_chart = go.Figure()
+    time_series_chart.add_trace(go.Scatter(x=time_series_data['date'], y=time_series_data['value'], mode='lines'))
 
-@app.route('/dashboard_v1')
-def dashboard_v1():
+    time_series_chart_layout = go.Layout(
+        title='Time Series Chart',
+        xaxis=dict(title='Date'),
+        yaxis=dict(title='Value')
+    )
+    time_series_chart.update_layout(time_series_chart_layout)
+
+    # Convert the charts to HTML
+    bar_chart_html = bar_chart.to_html(full_html=False)
+    time_series_chart_html = opy.plot(time_series_chart, auto_open=False, output_type='div')
+
+    return render_template('index.html', bar_chart=bar_chart_html, time_series_chart=time_series_chart_html)
+
+@app.route('/hat_duruslari')
+def hat_duruslari():
     # Create sample data
     data = pd.DataFrame({
         'category': ['A', 'B', 'C', 'D'],
@@ -64,9 +79,23 @@ def dashboard_v2():
         'value': (20 + 2 * np.random.randn(100)).cumsum()
     })
 
-    time_series_chart = px.line(time_series_data, x='date', y='value')
+    # time_series_chart = px.line(time_series_data, x='date', y='value')
 
-    return render_template('index2.html', bar_chart=bar_chart.to_html(), time_series_chart=time_series_chart.to_html())
+    time_series_chart = go.Figure()
+    time_series_chart.add_trace(go.Scatter(x=time_series_data['date'], y=time_series_data['value'], mode='lines'))
+
+    time_series_chart_layout = go.Layout(
+        title='Time Series Chart',
+        xaxis=dict(title='Date'),
+        yaxis=dict(title='Value')
+    )
+    time_series_chart.update_layout(time_series_chart_layout)
+
+    # Convert the charts to HTML
+    bar_chart_html = bar_chart.to_html(full_html=False)
+    time_series_chart_html = opy.plot(time_series_chart, auto_open=False, output_type='div')
+
+    return render_template('index2.html', bar_chart=bar_chart_html, time_series_chart=time_series_chart_html)
 
 @app.route('/dashboard_v3')
 def dashboard_v3():
