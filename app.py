21bea930c652d664bb8e30a63bf5ad5377569f3d
Mehmet Yiğit Köksal <yigitkoksal8@gmail.com>
2023-05-05 11:11:03 +0300

    -Make containers unsortable, to enable smooth dynamic chart operations of plotly\n -Add first real IOT sensor time-series chart (on sag biyel sicakligi) to second dashboard section on sidebar\n -Add dropdown list for this second dashboard for changing chart to other sensors\n -Add sub items on sidebar for each production line\n -Add new files, sensor data\n -Translate all remaining english phrases to turkish\n -Remove template Javascript chart codes\n -Remove redundant style codes to have neat code\n -Clean app.py from redundant codes, to have neat code

--- app.py
+++ app.py
@@ -4,26 +4,17 @@ import pandas as pd
 import numpy as np
 import plotly.graph_objs as go
 import plotly.offline as opy
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
-    # Create sample data
-    data = pd.DataFrame({
-        'category': ['A', 'B', 'C', 'D'],
-        'value': [10, 15, 13, 17]
-    })
-
-    # Create a bar chart
-    bar_chart = px.bar(data, x='category', y='value')
-
     # Create a time series chart
     time_series_data = pd.DataFrame({
         'date': pd.date_range(start='1/1/2020', periods=100, freq='M'),
         'value': (20 + 2 * np.random.randn(100)).cumsum()
     })
-
-    # time_series_chart = px.line(time_series_data, x='date', y='value')
 
     time_series_chart = go.Figure()
     time_series_chart.add_trace(go.Scatter(x=time_series_data['date'], y=time_series_data['value'], mode='lines'))
@@ -36,22 +27,13 @@ def index():
     time_series_chart.update_layout(time_series_chart_layout)
 
     # Convert the charts to HTML
-    bar_chart_html = bar_chart.to_html(full_html=False)
     time_series_chart_html = opy.plot(time_series_chart, auto_open=False, output_type='div')
 
-    return render_template('index.html', bar_chart=bar_chart_html, time_series_chart=time_series_chart_html)
+    return render_template('index.html', time_series_chart=time_series_chart_html)
+
 
 @app.route('/hat_duruslari')
 def hat_duruslari():
-    # Create sample data
-    data = pd.DataFrame({
-        'category': ['A', 'B', 'C', 'D'],
-        'value': [10, 15, 13, 17]
-    })
-
-    # Create a bar chart
-    bar_chart = px.bar(data, x='category', y='value')
-
     # Create a time series chart
     time_series_data = pd.DataFrame({
         'date': pd.date_range(start='1/1/2020', periods=100, freq='M'),
@@ -60,54 +42,34 @@ def hat_duruslari():
 
     time_series_chart = px.line(time_series_data, x='date', y='value')
 
-    return render_template('index.html', bar_chart=bar_chart.to_html(), time_series_chart=time_series_chart.to_html())
+    return render_template('index.html', time_series_chart=time_series_chart.to_html())
+
 
 @app.route('/dashboard_v2')
 def dashboard_v2():
-    # Create sample data
-    data = pd.DataFrame({
-        'category': ['A', 'B', 'C', 'D'],
-        'value': [10, 15, 13, 17]
-    })
-
-    # Create a bar chart
-    bar_chart = px.bar(data, x='category', y='value')
-
-    # Create a time series chart
-    time_series_data = pd.DataFrame({
-        'date': pd.date_range(start='1/1/2020', periods=100, freq='M'),
-        'value': (20 + 2 * np.random.randn(100)).cumsum()
-    })
-
-    # time_series_chart = px.line(time_series_data, x='date', y='value')
-
+    time_series_data = pd.read_csv("static/dist/pd/MTP101_onsagbiyel.csv")
+    time_series_data.rename(columns={"101-9": "value"}, inplace=True)
+    # time_series_data = pd.read_csv("static/dist/pd/MTP102_onsagbiyel.csv")
+    time_series_data["date"] = pd.to_datetime(time_series_data["date"])
+    time_series_data = time_series_data.loc[time_series_data["date"] <= np.datetime64("2023-02-04T13:13:00")]
     time_series_chart = go.Figure()
     time_series_chart.add_trace(go.Scatter(x=time_series_data['date'], y=time_series_data['value'], mode='lines'))
 
     time_series_chart_layout = go.Layout(
-        title='Time Series Chart',
-        xaxis=dict(title='Date'),
-        yaxis=dict(title='Value')
+        title='Ön Sağ Biyel Sıcaklığı(°C)',
+        xaxis=dict(title='Tarih'),
+        yaxis=dict(title='Değer')
     )
     time_series_chart.update_layout(time_series_chart_layout)
 
     # Convert the charts to HTML
-    bar_chart_html = bar_chart.to_html(full_html=False)
     time_series_chart_html = opy.plot(time_series_chart, auto_open=False, output_type='div')
 
-    return render_template('index2.html', bar_chart=bar_chart_html, time_series_chart=time_series_chart_html)
+    return render_template('index2.html', time_series_chart=time_series_chart_html)
+
 
 @app.route('/dashboard_v3')
 def dashboard_v3():
-    # Create sample data
-    data = pd.DataFrame({
-        'category': ['A', 'B', 'C', 'D'],
-        'value': [10, 15, 13, 17]
-    })
-
-    # Create a bar chart
-    bar_chart = px.bar(data, x='category', y='value')
-
     # Create a time series chart
     time_series_data = pd.DataFrame({
         'date': pd.date_range(start='1/1/2020', periods=100, freq='M'),
@@ -116,7 +78,8 @@ def dashboard_v3():
 
     time_series_chart = px.line(time_series_data, x='date', y='value')
 
-    return render_template('index3.html', bar_chart=bar_chart.to_html(), time_series_chart=time_series_chart.to_html())
+    return render_template('index3.html', time_series_chart=time_series_chart.to_html())
+
 
 if __name__ == '__main__':
     app.run(debug=True)
